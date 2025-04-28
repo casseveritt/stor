418f8c15bb7cc65d43078e4b2bf2194baac06939
Cass Everitt <cass@xyzw.us>
2025-04-27 19:52:15 -0500

    update link2filestore and delete deprecated scripts

--- scripts/link2filestore.py
+++ scripts/link2filestore.py
@@ -3,21 +3,19 @@
 import re
 import os
 import sys
-import shutil
 import sqlite3
 
-if len(sys.argv) != 4:
-    print("usage: link2filestore <db> <filestore> <linkdir>")
+if len(sys.argv) != 3:
+    print("usage: link2filestore <bkupdir> <linkdir>")
     sys.exit(0)
 
-db = sys.argv[1]
-filestore = sys.argv[2]
-linkdir = sys.argv[3]
+bkupdir = sys.argv[1]
+linkdir = sys.argv[2]
 
-con = sqlite3.connect(db)
+con = sqlite3.connect(os.path.join(bkupdir, "db"))
 cur = con.cursor()
 
-files = cur.execute("select * from b3sums order by relpath").fetchall()
+files = cur.execute("select b3sum, relpath from b3sums order by relpath").fetchall()
 
 count = len(files)
 
@@ -30,5 +28,5 @@ for file in files:
         if not os.path.exists(os.path.dirname(fp)):
             print(f"{count} making {os.path.dirname(fp)}")
             os.makedirs(os.path.dirname(fp))
-        fstorepath = os.path.join(filestore, b3sum[:2], b3sum)
+        fstorepath = os.path.join(bkupdir, "files", b3sum[:2], b3sum)
         os.link(fstorepath, fp)
