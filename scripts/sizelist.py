4d01614818bc705e86f5c7a3d33b5bf45ce0a359
Cass Everitt <cass.everitt@gmail.com>
2025-04-30 23:37:11 -0500

    sizelist

---
+++ scripts/sizelist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print("usage: sizelist.py <bkupdir>")
+    sys.exit(0)
+
+bkupdir = sys.argv[1]
+    
+file_list = []
+
+count = 0
+for path, dirs, files in os.walk(bkupdir):
+    for f in files:
+        count += 1
+        fp = os.path.join(path, f)
+        file_list.append([os.path.getsize(fp), fp])
+
+file_list.sort()
+
+count = len(file_list)
+for f in file_list:
+    count -= 1
+    print(f) 
