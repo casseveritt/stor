fe3fe773db41c5acaa731594cc25d87889410395
Cass Everitt <cass.everitt@gmail.com>
2025-04-30 23:54:30 -0500

    add checkb3

---
+++ scripts/checkb3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+def escape_bash_special_chars(text):
+    t2 = re.sub(r"([\$\"])", r"\\\1", text)
+    return t2
+
+if len(sys.argv) != 2:
+    print("usage: sizelist.py <bkupdir>")
+    sys.exit(0)
+
+bkupdir = sys.argv[1]
+    
+file_list = []
+
+for path, dirs, files in os.walk(bkupdir):
+    for f in files:
+        fp = os.path.join(path, f)
+        file_list.append([os.path.getsize(fp), fp])
+
+file_list.sort()
+
+count = len(file_list)
+for f in file_list:
+    count -= 1
+    if count % 1000 == 0:
+        print(count)
+    b3sum = os.popen(f'b3sum "{f[1]}"').read().split(' ')[0]
+    if b3sum != os.path.basename(f[1]):
+        print(b3sum, os.path.basename(f[1])) 
